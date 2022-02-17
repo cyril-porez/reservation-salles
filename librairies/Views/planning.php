0a57a4fe3af54760414389f8956748d4d0e73682
dark sassuke <mohamad.sheikho@laplateforme.io>
2022-02-17 23:40:42 +0100

    page terminé sauf footer

--- librairies/Views/planning.php
+++ librairies/Views/planning.php
@@ -1,51 +1,66 @@
 <?php
-    //require_once('../Controller/Reservation.php');
-    require_once('../autoload.php');
+require_once('../autoload.php');
 
-    session_start();
-    if (!isset($_SESSION['date'])) {
-        $_SESSION['date'] = 0;
-    }
+session_start();
+if (!isset($_SESSION['date'])) {
+    $_SESSION['date'] = 0;
+}
 
 
-    if (isset($_GET['previous'])) {
-        $_SESSION['date'] = $_SESSION['date'] - 7;
-   }
-   else if (isset($_GET['next'])) {
-        $_SESSION["date"] = $_SESSION['date'] + 7;
-   }
+if (isset($_GET['previous'])) {
+    $_SESSION['date'] = $_SESSION['date'] - 7;
+} else if (isset($_GET['next'])) {
+    $_SESSION["date"] = $_SESSION['date'] + 7;
+}
 
-   $days = ["Lundi ", "Mardi ", "Mercredi ", "Jeudi ", "Vendredi ", "Samedi ", "Dimanche "];
+$days = ["Lundi ", "Mardi ", "Mercredi ", "Jeudi ", "Vendredi ", "Samedi ", "Dimanche "];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="librairies/views/css/planning.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
+    <title>Document</title>
+</head>
 
 
-<body id="body_planning">
+<body>
     <header>
         <?php require_once('header.php'); ?>
     </header>
-    <main>
-        <h1>Planning</h1>
-        
-            <button><a href="planning.php?previous">précédent</a></button>
-            <button><a href="planning.php?next">suivant</a></button>
 
-        <table>
+    <h1>Planning</h1>
+
+    <div class="btn">
+        <button><a href="planning.php?previous">précédent</a></button>
+        <button><a href="planning.php?next">suivant</a></button>
+    </div>
+    <main>
+
+        <table class="table-style">
             <thead>
                 <th></th>
-                <?php for ($i = 0; $i < 7; $i++): ?>
-<<<<<<< HEAD
-                  <th><?= $days[$i]; echo date("d-m-y", strtotime('Monday this week +'.($i + $_SESSION['date']).'days')); ?></th>
-=======
-                    <th><?= date("d-m-y", strtotime('Monday this week +'.($i + $_SESSION['date']).'days')); ?></th>
->>>>>>> 426d429fea7e8fff16ed6c4ad063e206f770f821
-                <?php endfor ; ?>
+                <?php for ($i = 0; $i < 7; $i++) : ?>
+                    <th><?= $days[$i];
+                        echo date("d-m-y", strtotime('Monday this week +' . ($i + $_SESSION['date']) . 'days')); ?></th>
+                <?php endfor; ?>
             </thead>
             <tbody>
                 <?php
-                    $reservation = new \Controllers\Reservation();
-                    $reservation->displayPlanning();
+                $reservation = new \Controllers\Reservation();
+                $reservation->displayPlanning();
                 ?>
             </tbody>
         </table>
+
     </main>
+    <footer>
+    <?php
+    require_once('footer.php');
+    ?>
+    </footer>
 </body>
